49c78d01ce341dd68de98ec80a5f0dd8813bf83a
Ruslan Orlov <org1978org@gmail.com>
2025-04-03 22:48:45 +0500

    Commit changes of refresh access token

--- src/main/java/org/oauth2client2/services/CheckConsentService.java
+++ src/main/java/org/oauth2client2/services/CheckConsentService.java
@@ -1,6 +1,7 @@
 package org.oauth2client2.services;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -10,6 +11,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CheckConsentService {
@@ -21,35 +23,40 @@ public class CheckConsentService {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         if (authentication != null) {
-            String principalName = authentication.getName();
-            HttpHeaders headers = new HttpHeaders();
-            headers.setBearerAuth(tokenService.getAccessToken());
+            try {
+                String principalName = authentication.getName();
+                HttpHeaders headers = new HttpHeaders();
+                headers.setBearerAuth(tokenService.getAccessToken());
 
-            HttpEntity<String> entity = new HttpEntity<>(principalName, headers);
+                HttpEntity<String> entity = new HttpEntity<>(principalName, headers);
 
-            ResponseEntity<Boolean> response =
-                    restTemplate.exchange(
-                            "http://authserver:9000/oauth2/check-consent",
-                            HttpMethod.POST,
-                            entity,
-                            Boolean.class
-                    );
+                ResponseEntity<Boolean> response =
+                        restTemplate.exchange(
+                                "http://authserver:9000/oauth2/check-consent",
+                                HttpMethod.POST,
+                                entity,
+                                Boolean.class
+                        );
 
-            /*HttpEntity<Object> entity = new HttpEntity<>(headers);
+                /*HttpEntity<Object> entity = new HttpEntity<>(headers);
 
-            ResponseEntity<Boolean> response =
-                    restTemplate.exchange(
-                            "http://authserver:9000/oauth2/check-consent?principalName=" + principalName,
-                            HttpMethod.GET,
-                            entity,
-                            Boolean.class
-                    );*/
+                ResponseEntity<Boolean> response =
+                        restTemplate.exchange(
+                                "http://authserver:9000/oauth2/check-consent?principalName=" + principalName,
+                                HttpMethod.GET,
+                                entity,
+                                Boolean.class
+                        );*/
 
-            if (response.getStatusCode().is2xxSuccessful()) {
-                return response.getBody();
+                if (response.getStatusCode().is2xxSuccessful()) {
+                    return response.getBody();
+                }
+            } catch (Exception ex) {
+                log.error("Ошибка при проверке согласия", ex);
+                return false;
             }
         }
-
+        // Возвращаем false, если аутентификация отсутствует
         return false;
     }
 
--- src/main/java/org/oauth2client2/services/MyTokenService.java
+++ src/main/java/org/oauth2client2/services/MyTokenService.java
@@ -1,33 +1,100 @@
 package org.oauth2client2.services;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizationContext;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
 
 @Service
 @RequiredArgsConstructor
 public class MyTokenService {
 
     private final OAuth2AuthorizedClientService authorizedClientService;
+    private final OAuth2AuthorizedClientProvider authorizedClientProvider;
 
     public String getAccessToken() {
-        String accessToken = null;
+        OAuth2AuthorizedClient authorizedClient = null;
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
+        // Загружаем существующего авторизованного клиента
         if (authentication instanceof OAuth2AuthenticationToken oauthToken) {
-            OAuth2AuthorizedClient client = authorizedClientService.loadAuthorizedClient(
+            authorizedClient = authorizedClientService.loadAuthorizedClient(
                     oauthToken.getAuthorizedClientRegistrationId(),
                     oauthToken.getName()
             );
-            if (client != null) {
-                accessToken = client.getAccessToken().getTokenValue();
-            }
         }
-        return accessToken;
+
+        // Если авторизованного клиента не существует, выбрасываем исключение
+        if (authorizedClient == null) {
+            throw new AuthenticationServiceException("Unable to load OAuth2 client");
+        }
+
+        // Если токен авторизованного клиента просрочен, обновляем его и возвращаем
+        if (isTokenExpired(authorizedClient.getAccessToken())) {
+            return refreshAccessToken();
+        }
+
+        // Или возвращаем токен авторизованного клиента без обновления
+        return authorizedClient.getAccessToken().getTokenValue();
     }
 
+    private boolean isTokenExpired(OAuth2AccessToken accessToken) {
+        Instant now = Instant.now();
+        Instant expiresAt = accessToken.getExpiresAt();
+        if (expiresAt == null) {
+            // Более консервативный подход - считаем токен истекшим,
+            // если нет информации о его сроке действия
+            // =======================================================
+            // Хотя стандартная интерпретация в Spring Security OAuth2
+            // воспринимает отсутствие информации о сроке действия как
+            // "токен не имеет срока действия" или "токен действителен бессрочно"
+            return true;
+        }
+        return now.plusSeconds(60).isAfter(expiresAt);
+    }
+
+    public String refreshAccessToken() {
+        OAuth2AuthorizedClient authorizedClient = null;
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        // Загружаем существующего авторизованного клиента
+        if (authentication instanceof OAuth2AuthenticationToken oauthToken) {
+            authorizedClient = authorizedClientService.loadAuthorizedClient(
+                    oauthToken.getAuthorizedClientRegistrationId(),
+                    oauthToken.getName()
+            );
+        }
+
+        // Если авторизованного клиента не существует, выбрасываем исключение
+        if (authorizedClient == null) {
+            throw new AuthenticationServiceException("Unable to load OAuth2 client");
+        }
+
+        // Создаем контекст авторизации с существующим клиентом
+        OAuth2AuthorizationContext authorizationContext = OAuth2AuthorizationContext
+                .withAuthorizedClient(authorizedClient)
+                .principal(authentication)
+                .build();
+
+        // Попытка авторизации с обновлением токена
+        OAuth2AuthorizedClient refreshedClient = authorizedClientProvider.authorize(authorizationContext);
+
+        // Проверяем успешность обновления токена
+        if (refreshedClient != null) {
+            // Возвращаем новый токен доступа
+            return refreshedClient.getAccessToken().getTokenValue();
+        }
+
+        // Выбрасываем исключение, если обновление не удалось
+        throw new AuthenticationServiceException("Unable to refresh access token");
+    }
 }
